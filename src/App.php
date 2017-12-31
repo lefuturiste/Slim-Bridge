5f4f3f150b153acebffcc2c9b6d0c200a25f8814
Le_Futuriste <spamfree@matthieubessat.fr>
2017-12-31 10:06:39 +0100

    Revert changes

--- src/App.php
+++ src/App.php
@@ -12,72 +12,27 @@ use DI\ContainerBuilder;
  */
 class App extends \Slim\App
 {
-	protected $bundles = [];
-	protected $hasBundles = false;
-	protected $twigPaths= ['../App/views'];
+    public function __construct()
+    {
+        $containerBuilder = new ContainerBuilder;
+        $containerBuilder->addDefinitions(__DIR__ . '/config.php');
+        $this->configureContainer($containerBuilder);
+        $container = $containerBuilder->build();
 
-	public function __construct()
-	{
-		//on initialise le container
-		$containerBuilder = new ContainerBuilder;
-		$containerBuilder->addDefinitions(__DIR__ . '/config.php');
+        parent::__construct($container);
+    }
 
-		if ($this->hasBundles) {
-			//pour chaque bundle, on l'initialise
-			foreach ($this->getBundles() as $bundle) {
-				$bundle = new $bundle($this);
-				array_push($this->bundles, $bundle);
-				//on configure les container pour ce bundle
-				$bundle->configureContainer($containerBuilder);
-				//on ajoute le path twig à ceux existant
-				$this->twigPaths = $bundle->getTwigPaths($this->twigPaths, $containerBuilder);
-			}
-		}
-
-		//on configure les container de l'app principale
-		$this->configureContainer($containerBuilder);
-		//on met les twig path dans les container
-		$this->configureTwigPathArray($containerBuilder);
-		$container = $containerBuilder->build();
-
-		parent::__construct($container);
-
-		if ($this->hasBundles) {
-			// on enregistre les routes des bundles
-			$this->registerBundlesRoutes();
-		}
-	}
-
-	/**
-	 * Override this method to configure the container builder.
-	 *
-	 * For example, to load additional configuration files:
-	 *
-	 *     protected function configureContainer(ContainerBuilder $builder)
-	 *     {
-	 *         $builder->addDefinitions(__DIR__ . 'my-config-file.php');
-	 *     }
-	 */
-	protected function configureContainer(ContainerBuilder $builder)
-	{
-	}
-
-	public function getBundles()
-	{
-		return [];
-	}
-	
-	protected function configureTwigPathArray(ContainerBuilder $containerBuilder)
-	{
-		$containerBuilder->addDefinitions([
-			'twig_paths' => $this->twigPaths
-		]);
-	}
-
-	protected function registerBundlesRoutes()
-	{
-		foreach ($this->bundles as $bundle) {
-			$bundle->routes();
-		}
-	}
+    /**
+     * Override this method to configure the container builder.
+     *
+     * For example, to load additional configuration files:
+     *
+     *     protected function configureContainer(ContainerBuilder $builder)
+     *     {
+     *         $builder->addDefinitions(__DIR__ . 'my-config-file.php');
+     *     }
+     */
+    protected function configureContainer(ContainerBuilder $builder)
+    {
+    }
 }
